38d9ac74d8a8dc25e74dae9d07edc25a321ca8ee
Tingkai Liu <tl2747@columbia.edu>
2021-11-04 20:25:20 -0400

    fix manifest to include macros and theme. update cli with more options. remove some unncessary macros.

--- latex_to_myst/cli.py
+++ latex_to_myst/cli.py
@@ -1,8 +1,19 @@
 import shutil
 import argparse
 import subprocess
+import glob
 from pathlib import Path
+from typing import Type
 
+def _validate_file(path: str, file_ext: str, check_exist: bool =True) -> str:
+    """Validate file path according to file_ext"""
+    if not Path(path).suffix:
+        path += file_ext
+    if check_exist and not Path(path).exists():
+        raise RuntimeError(f"File '{path}' not found.")
+    if Path(path).suffix != file_ext:
+        raise RuntimeError(f"File '{path}' extension does not match desired extension '{file_ext}'. ")
+    return path
 
 def main():
     """Main CLI Entry Point to Latex-to-Myst
@@ -17,6 +28,14 @@ def main():
         $ latex2myst -h
     """
     parser = argparse.ArgumentParser(description="Convert LaTeX to MyST")
+    parser.add_argument(
+        "macro_files",
+        metavar="macros",
+        default=None,
+        type=str,
+        nargs="*",
+        help="Names of files of macros that you'd like to use",
+    )
     parser.add_argument("file_in", metavar="input", type=str, help="Input LaTeX file")
     parser.add_argument(
         "file_out", metavar="output", type=str, help="Output Markdown file"
@@ -29,6 +48,13 @@ def main():
     #     help="Logging level, default to None which turns off logging.",
     # )
     parser.add_argument(
+        "-dm",
+        "--default_macros",
+        type=bool,
+        default=True,
+        help="Whether to use default macro.",
+    )
+    parser.add_argument(
         "-hl",
         "--highlight",
         action="store_true",
@@ -40,17 +66,27 @@ def main():
     if not shutil.which("pandoc"):
         raise ModuleNotFoundError("Pandoc >= 2.11 required.")
 
-    fi = Path(args.file_in)
-    fo = Path(args.file_out)
+    custom_macros = []
+    if args.macro_files is not None:
+        if isinstance(args.macro_files, str):
+            custom_macros = [args.macro_files]
+        else:
+            custom_macros = args.macro_files
+
+    macro_paths = []
+    for fname in custom_macros:
+        fname = _validate_file(fname, '.tex')
+        macro_paths.append(Path(fname))
+
     CURR_PATH = Path(__file__).parent.resolve()
 
-    assert fi.exists()
-    assert fi.suffix == ".tex" or fi.suffix == ""
-    assert fo.suffix == ".md" or fo.suffix == ""
+    fi = Path(_validate_file(args.file_in, '.tex'))
+    fo = Path(_validate_file(args.file_out, '.md', check_exist=False))
 
     cmd = [
         "pandoc",
-        str(CURR_PATH / "macros.tex"),
+        str(CURR_PATH / "macros.tex") if args.default_macros else "",
+        *[str(p) for p in macro_paths],
         str(fi),
         "-o",
         str(fo),
