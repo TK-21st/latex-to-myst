8dca0b4564d222c36315731f5fd360509c8cd35d
Tingkai Liu <tl2747@columbia.edu>
2021-11-04 21:10:06 -0400

    Bump version: 0.0.3 → 0.0.4

--- latex_to_myst/__init__.py
+++ latex_to_myst/__init__.py
@@ -3,6 +3,6 @@
 
 __author__ = """Tingkai Liu"""
 __email__ = "tingkai.liu.21st@gmail.com"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 from latex_to_myst.cli import main
--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/TK-21st/latex-to-myst",
-    version="0.0.3",
+    version="0.0.4",
     zip_safe=False,
 )
