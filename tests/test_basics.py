86904c4c93ccd50cebf67e1b34561f2f22c853c6
Tingkai Liu <tl2747@columbia.edu>
2021-11-04 20:42:16 -0400

    update test to remove temp files

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -11,12 +11,16 @@ CURR_DIR = Path(__file__).parent
 
 @pytest.mark.parametrize("block_type", ["figure", "math", "amsthm"])
 def test_basics(block_type):
-    out = tempfile.NamedTemporaryFile("w+", dir=str(CURR_DIR))
-    subprocess.run(
-        ["latex2myst", str(CURR_DIR / "sample_files" / f"{block_type}.tex"), out.name],
-        check=True,
-    )
-    out_lines = out.readlines()
-    print(out_lines)
-    with open(str(CURR_DIR / "sample_files" / f"{block_type}.md")) as f:
-        assert f.readlines() == out_lines, "".join(ndiff(f.readlines(), out_lines))
+    with tempfile.TemporaryFile("w+", dir=str(CURR_DIR)) as out:
+        subprocess.run(
+            [
+                "latex2myst",
+                str(CURR_DIR / "sample_files" / f"{block_type}.tex"),
+                f"{out.name}.md",
+            ],
+            check=True,
+        )
+        with open(f"{out.name}.md", "r") as real_out:
+            out_lines = real_out.readlines()
+        with open(str(CURR_DIR / "sample_files" / f"{block_type}.md")) as f:
+            assert f.readlines() == out_lines, "".join(ndiff(f.readlines(), out_lines))
